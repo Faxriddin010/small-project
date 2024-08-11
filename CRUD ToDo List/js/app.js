2d0e9c4d5fe5d815e0ad8ac4d2c7c5180ce52849
Faxriddin Baxtiyorov <bahtiyorovf96@gmail.com>
2024-08-11 05:49:04 +0500

    CRUD ToDo List

---
+++ CRUD ToDo List/js/app.js
@@ -0,0 +1,191 @@
+const formCreate = document.getElementById('form-create')
+const formEdit = document.getElementById('form-edit')
+const listGroupTodo = document.getElementById('list-group-todo')
+const messageCreate = document.getElementById('message-create')
+const time = document.getElementById('time')
+const modal = document.getElementById('modal')
+const overlay = document.getElementById('overlay')
+/* time elements */
+const fullDay = document.getElementById('full-day')
+const hourEl = document.getElementById('hour')
+const minuteEl = document.getElementById('minute')
+const secondEl = document.getElementById('second')
+const closeEl = document.getElementById('close')
+
+let editItemId
+
+//check 
+let todos = JSON.parse(localStorage.getItem('list'))
+    ? JSON.parse(localStorage.getItem('list'))
+    : []
+
+if (todos.length) showTodos()
+
+// set todos to localStorage
+function setTodos() {
+    localStorage.setItem('list', JSON.stringify(todos))
+}
+
+// time
+function getTime() {
+    const now = new Date()
+    const date = now.getDate() < 10
+        ? '0' + now.getDate()
+        : now.getDate()
+    const month = now.getMonth() < 10
+        ? '0' + (now.getMonth() + 1)
+        : now.getMonth()
+    const year = now.getFullYear()
+
+    const hour = now.getHours() < 10
+        ? '0' + now.getHours()
+        : now.getHours()
+    const minute = now.getMinutes() < 10
+        ? '0' + now.getMinutes()
+        : now.getMinutes()
+    const second = now.getSeconds() < 10
+        ? '0' + now.getSeconds()
+        : now.getSeconds()
+
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+    const monthTitle = now.getMonth()
+    fullDay.textContent = `${date}-${months[monthTitle]} ${year}`
+
+    hourEl.textContent = hour
+    minuteEl.textContent = minute
+    secondEl.textContent = second
+
+    return `${hour}:${minute}, ${date}.${month}.${year} `
+
+}
+
+setInterval(getTime, 900)
+
+// show todos
+function showTodos() {
+    const todos = JSON.parse(localStorage.getItem('list'))
+
+    listGroupTodo.innerHTML = ''
+    todos.forEach((item, i) => {
+        listGroupTodo.innerHTML +=
+            `
+                <li class="list-group-item d-flex justify-content-between ${item.completed == true ? 'completed' : ''}" ondblclick=setCompleted(${i})>
+                    ${item.text}
+                    <div class="todo-icons">
+                        <span class="opacity-50 me-2">${item.time}</span>
+                        <img onclick=(editTodo(${i})) src="img/edit.svg" alt="edit icon" width="25" height="25" />
+                        <img
+                            onclick=(deleteTodo(${i}))
+                            src="img/delete.svg"
+                            alt="delete icon"
+                            width="25"
+                            height="25"
+                        />
+                    </div>
+                </li>
+            `
+    })
+}
+
+// show error message function
+function showMessage(where, message) {
+    document.getElementById(`${where}`).textContent = message
+    setTimeout((() => messageCreate.textContent = ''), 2500)
+}
+
+// get Todos
+formCreate.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const todoText = formCreate['input-create'].value.trim()
+    formCreate.reset()
+    if (todoText.length) {
+        todos.push({ text: todoText, time: getTime(), completed: false })
+        setTodos()
+        showTodos()
+    } else {
+        showMessage('message-create', 'Please, enter your todo text...')
+    }
+})
+
+
+// delete todo
+function deleteTodo(idx) {
+    const deletedTodos = todos.filter((item, i) => {
+        return i !== idx
+    })
+
+    todos = deletedTodos
+    setTodos()
+    showTodos()
+}
+
+// set Completed 
+function setCompleted(idx) {
+    const completedTodos = todos.map((item, i) => {
+        if (idx == i) {
+            return { ...item, completed: item.completed == true ? false : true }
+        } else {
+            return { ...item }
+        }
+    })
+
+    todos = completedTodos
+    setTodos()
+    showTodos()
+}
+
+//edit Form
+formEdit.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const todoText = formEdit['input-edit'].value.trim()
+    formEdit.reset()
+    if (todoText.length) {
+        todos.splice(editItemId, 1, {
+            text: todoText,
+            time: getTime(),
+            completed: false
+        })
+        setTodos()
+        showTodos()
+        close()
+    } else {
+        showMessage('message-edit', 'Please, enter your todo text...')
+    }
+})
+
+// edit Todo
+function editTodo(idx) {
+    open()
+    editItemId = idx
+}
+
+overlay.addEventListener('click', close)
+closeEl.addEventListener('click', close)
+document.addEventListener('keydown', (e) => {
+    if (e.which === 27) {
+        close()
+    }
+})
+
+function open() {
+    modal.classList.toggle('hidden')
+    overlay.classList.toggle('hidden')
+}
+
+function close() {
+    modal.classList.toggle('hidden')
+    overlay.classList.toggle('hidden')
+}
